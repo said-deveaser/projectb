8bf50f441bf1932ddd9e8e96964a512fb6c70caf
cherlin <alright.said.offcl@gmail.com>
2022-05-10 18:07:22 +0000

    feat: добавил автодокументацию в ввиде json

--- src/api/routers/auth/authRouter.ts
+++ src/api/routers/auth/authRouter.ts
@@ -5,6 +5,12 @@ export namespace Auth {
   export const router = express.Router()
 
   router.get("/token", (req,res) => {
+ // #swagger.description = 'Get all todos'
+ /* #swagger.responses[200] = {
+     description: 'Array of all todos',
+     schema: { $ref: '#/definitions/Todos' }
+ } */
+
     res.send("new_token")
   })
 }
--- swagger/index.ts
+++ swagger/index.ts
@@ -1,16 +1,52 @@
 import { join, dirname } from 'path'
 import { fileURLToPath } from 'url'
+// @ts-ignore
 import swaggerAutogen from 'swagger-autogen'
 
-// const _dirname = dirname(fileURLToPath(import.meta.url))
+const _dirname = __dirname
 
-// const doc = ...
+const doc = {
+    // общая информация
+    info: {
+      title: 'Todo API',
+      description: 'My todo API'
+    },
+    // что-то типа моделей
+    definitions: {
+      // модель задачи
+      Todo: {
+        id: '1',
+        text: 'test',
+        done: false
+      },
+      // модель массива задач
+      Todos: [
+        {
+          // ссылка на модель задачи
+          $ref: '#/definitions/Todo'
+        }
+      ],
+      // модель объекта с текстом новой задачи
+      Text: {
+        text: 'test'
+      },
+      // модель объекта с изменениями существующей задачи
+      Changes: {
+        changes: {
+          text: 'test',
+          done: true
+        }
+      }
+    },
+    host: 'localhost:4000',
+    schemes: ['http']
+   }
 
 // путь и название генерируемого файла
-// const outputFile = join(_dirname, 'output.json')
+const outputFile = join(_dirname, 'output.json')
 // массив путей к роутерам
-// const endpointsFiles = [join(_dirname, '../server.js')]
+const endpointsFiles = [join(_dirname, '../src/api/routers/auth/authRouter.ts')]
 
-// swaggerAutogen(/*options*/)(outputFile, endpointsFiles, doc).then(({ success }) => {
-//   console.log(`Generated: ${success}`)
-// })
+swaggerAutogen(/*options*/)(outputFile, endpointsFiles, doc).then(({ success }:any) => {
+  console.log(`Generated: ${success}`)
+})
